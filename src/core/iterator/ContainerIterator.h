109b901d59fbf07f50e23929cb01d981fb912975
Marcus D. R. Klarqvist <mk21@sanger.ac.uk>
2017-12-01 11:49:40 +0000

    Update container iterator

--- src/core/iterator/ContainerIterator.h
+++ src/core/iterator/ContainerIterator.h
@@ -8,6 +8,8 @@ namespace Iterator{
 class ContainerIteratorDataInterface{
 private:
 	typedef ContainerIteratorDataInterface self_type;
+
+protected:
 	typedef IO::BasicBuffer buffer_type;
 
 public:
@@ -74,7 +76,7 @@ private:
 	typedef StreamContainerHeader header_type;
 
 public:
-	ContainerIteratorData();
+	ContainerIteratorData(buffer_type& buffer, header_type& header);
 	virtual ~ContainerIteratorData();
 
 public:
@@ -88,7 +90,7 @@ private:
 	typedef StreamContainerHeaderStride header_type;
 
 public:
-	ContainerIteratorStride();
+	ContainerIteratorStride(buffer_type& buffer, header_type& header);
 	virtual ~ContainerIteratorStride();
 
 public:
@@ -98,34 +100,110 @@ public:
 class ContainerIterator{
 private:
 	typedef ContainerIterator self_type;
-	typedef ContainerIteratorData<BYTE> data_iterator_byte_type;
-	typedef ContainerIteratorData<U16> data_iterator_u16_type;
-	typedef ContainerIteratorData<U32> data_iterator_u32_type;
-	typedef ContainerIteratorData<U64> data_iterator_u64_type;
-	typedef ContainerIteratorData<char> data_iterator_char_type;
-	typedef ContainerIteratorData<S16> data_iterator_s16_type;
-	typedef ContainerIteratorData<S32> data_iterator_s32_type;
+	typedef ContainerIteratorData<BYTE>   data_iterator_byte_type;
+	typedef ContainerIteratorData<U16>    data_iterator_u16_type;
+	typedef ContainerIteratorData<U32>    data_iterator_u32_type;
+	typedef ContainerIteratorData<U64>    data_iterator_u64_type;
+	typedef ContainerIteratorData<char>   data_iterator_char_type;
+	typedef ContainerIteratorData<S16>    data_iterator_s16_type;
+	typedef ContainerIteratorData<S32>    data_iterator_s32_type;
+	typedef ContainerIteratorData<float>  data_iterator_float_type;
+	typedef ContainerIteratorData<double> data_iterator_double_type;
 	typedef ContainerIteratorStride<BYTE> stride_iterator_byte_type;
-	typedef ContainerIteratorStride<U16> stride_iterator_u16_type;
-	typedef ContainerIteratorStride<U32> stride_iterator_u32_type;
-	typedef ContainerIteratorStride<U64> stride_iterator_u64_type;
+	typedef ContainerIteratorStride<U16>  stride_iterator_u16_type;
+	typedef ContainerIteratorStride<U32>  stride_iterator_u32_type;
+	typedef ContainerIteratorStride<U64>  stride_iterator_u64_type;
 	typedef ContainerIteratorStride<char> stride_iterator_char_type;
-	typedef ContainerIteratorStride<S16> stride_iterator_s16_type;
-	typedef ContainerIteratorStride<S32> stride_iterator_s32_type;
+	typedef ContainerIteratorStride<S16>  stride_iterator_s16_type;
+	typedef ContainerIteratorStride<S32>  stride_iterator_s32_type;
+	typedef ContainerIteratorStride<float>   stride_iterator_float_type;
+	typedef ContainerIteratorStride<double>  stride_iterator_double_type;
 
 protected:
 	typedef StreamContainer container_type;
 
 public:
-	inline const bool isUniform(void) const{ return(this->container.header.controller.uniform); }
-	inline const bool isUniformStrides(void) const{ return(this->container.header_stride.controller.uniform); }
-	inline const bool hasStrides(void) const{ return(this->container.header.controller.mixedStride); }
+	explicit ContainerIterator(void) :
+		n_entries(0),
+		position(0),
+		container(nullptr),
+		data_iterator(nullptr),
+		stride_iterator(nullptr)
+	{
+
+	}
+	~ContainerIterator(){
+		// Do not delete container. It's allocated externally
+		delete this->data_iterator;
+		delete this->stride_iterator;
+	}
+
+	void operator()(container_type& c){
+		// Store container reference
+		this->container = &c;
+
+		// Recycling is possible
+		delete this->data_iterator;   this->data_iterator   = nullptr;
+		delete this->stride_iterator; this->stride_iterator = nullptr;
+
+		// Data iterator
+		if(this->container->header.controller.signedness == 0){
+			switch(this->container->header.controller.type){
+			case(Core::TYPE_8B)  : this->data_iterator = new data_iterator_byte_type(this->container->buffer_data_uncompressed, this->container->header); break;
+			case(Core::TYPE_16B) : this->data_iterator = new data_iterator_u16_type (this->container->buffer_data_uncompressed, this->container->header); break;
+			case(Core::TYPE_32B) : this->data_iterator = new data_iterator_u32_type (this->container->buffer_data_uncompressed, this->container->header); break;
+			case(Core::TYPE_64B) : this->data_iterator = new data_iterator_u64_type (this->container->buffer_data_uncompressed, this->container->header); break;
+			case(Core::TYPE_FLOAT)  : this->data_iterator = new data_iterator_float_type (this->container->buffer_data_uncompressed, this->container->header); break;
+			case(Core::TYPE_DOUBLE) : this->data_iterator = new data_iterator_double_type(this->container->buffer_data_uncompressed, this->container->header); break;
+			default: std::cerr << "illegal type!" << std::endl; exit(1); break;
+			}
+		} else {
+			switch(this->container->header.controller.type){
+			case(Core::TYPE_8B)  : this->data_iterator = new data_iterator_char_type(this->container->buffer_data_uncompressed, this->container->header); break;
+			case(Core::TYPE_16B) : this->data_iterator = new data_iterator_s16_type (this->container->buffer_data_uncompressed, this->container->header); break;
+			case(Core::TYPE_32B) : this->data_iterator = new data_iterator_s32_type (this->container->buffer_data_uncompressed, this->container->header); break;
+			case(Core::TYPE_FLOAT)  : this->data_iterator = new data_iterator_float_type (this->container->buffer_data_uncompressed, this->container->header); break;
+			case(Core::TYPE_DOUBLE) : this->data_iterator = new data_iterator_double_type(this->container->buffer_data_uncompressed, this->container->header); break;
+			default: std::cerr << "illegal type!" << std::endl; exit(1); break;
+			}
+		}
+
+		// Construct this iterator if there is a mixed stride
+		// otherwise we assume that the stride size is one (1)
+		if(this->container->header.controller.mixedStride){
+			// Stride iterator
+			if(this->container->header_stride.controller.signedness == 0){
+				switch(this->container->header_stride.controller.type){
+				case(Core::TYPE_8B)  : this->stride_iterator = new stride_iterator_byte_type(this->container->buffer_strides_uncompressed, this->container->header_stride); break;
+				case(Core::TYPE_16B) : this->stride_iterator = new stride_iterator_u16_type (this->container->buffer_strides_uncompressed, this->container->header_stride); break;
+				case(Core::TYPE_32B) : this->stride_iterator = new stride_iterator_u32_type (this->container->buffer_strides_uncompressed, this->container->header_stride); break;
+				case(Core::TYPE_64B) : this->stride_iterator = new stride_iterator_u64_type (this->container->buffer_strides_uncompressed, this->container->header_stride); break;
+				case(Core::TYPE_FLOAT)  : this->stride_iterator = new stride_iterator_float_type (this->container->buffer_strides_uncompressed, this->container->header_stride); break;
+				case(Core::TYPE_DOUBLE) : this->stride_iterator = new stride_iterator_double_type(this->container->buffer_strides_uncompressed, this->container->header_stride); break;
+				default: std::cerr << "illegal type!" << std::endl; exit(1); break;
+				}
+			} else {
+				switch(this->container->header_stride.controller.type){
+				case(Core::TYPE_8B)  : this->stride_iterator = new stride_iterator_char_type(this->container->buffer_strides_uncompressed, this->container->header_stride); break;
+				case(Core::TYPE_16B) : this->stride_iterator = new stride_iterator_s16_type (this->container->buffer_strides_uncompressed, this->container->header_stride); break;
+				case(Core::TYPE_32B) : this->stride_iterator = new stride_iterator_s32_type (this->container->buffer_strides_uncompressed, this->container->header_stride); break;
+				case(Core::TYPE_FLOAT)  : this->stride_iterator = new stride_iterator_float_type (this->container->buffer_strides_uncompressed, this->container->header_stride); break;
+				case(Core::TYPE_DOUBLE) : this->stride_iterator = new stride_iterator_double_type(this->container->buffer_strides_uncompressed, this->container->header_stride); break;
+				default: std::cerr << "illegal type!" << std::endl; exit(1); break;
+				}
+			}
+		}
+	}
+
+	inline const bool isUniform(void) const{ return(this->container->header.controller.uniform); }
+	inline const bool isUniformStrides(void) const{ return(this->container->header_stride.controller.uniform); }
+	inline const bool hasStrides(void) const{ return(this->container->header.controller.mixedStride); }
 
 public:
-	U32 n_entries;
-	U32 position;
-	container_type& container;
-	data_iterator_byte_type* data_iterator; // recast me as the correct base type
+	U32 n_entries; // number of entries
+	U32 position;  // iterator position
+	container_type* container;                  // reference container
+	data_iterator_byte_type* data_iterator;     // recast me as the correct base type
 	stride_iterator_byte_type* stride_iterator; // recast me as the correct base type
 };
 
