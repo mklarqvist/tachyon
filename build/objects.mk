71b816882d0381c8ed318532b7746ed3a4176e28
Marcus D. R. Klarqvist <mk819@cam.ac.uk>
2018-01-25 17:19:52 +0000

    update reorganization

--- build/objects.mk
+++ build/objects.mk
@@ -2,7 +2,7 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-# Add inputs and outputs from these tool invocations to the build variables 
+USER_OBJS :=
 
-# Each subdirectory must supply rules for building sources it contributes
+LIBS := -lzstd -lcrypto
 
